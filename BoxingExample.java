feaa3599be0d3744e9b94292ef1c61a46d83a7bb
SwarupaDey <88048288+HappySwarupa@users.noreply.github.com>
2022-11-20 11:57:25 +0530

    Add files via upload

---
+++ BoxingExample.java
@@ -0,0 +1,14 @@
+package corejava;
+import java.util.*;
+public class BoxingExample {
+	
+	public static void main(String args[]) {
+		
+		int a= 4;
+		Integer b= new Integer(a);
+		Integer c=5;
+		System.out.println(b+" "+c);
+		
+	}
+
+}
