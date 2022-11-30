c9847f1575d45f588f6933a0f298c6940ed95485
SwarupaDey <88048288+HappySwarupa@users.noreply.github.com>
2022-11-30 19:58:21 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,20 @@
+package corejava;
+import java.util.*;
+public class ArrayExample {
+
+	public static void main(String args[])	{
+		int[]a=new int[3];
+		a[0]=  2;
+		a[1]=  3;
+		a[2]= 4;
+		System.out.println("output of the array");
+		
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
+
