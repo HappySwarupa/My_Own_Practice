764ba68daff3295b4546f8e5e237885261362957
SwarupaDey <88048288+HappySwarupa@users.noreply.github.com>
2022-12-02 13:31:03 +0530

    Add files via upload

---
+++ AscendingOrderDemo.java
@@ -0,0 +1,14 @@
+/*example of ascending order*/
+package corejava;
+import java.util.Arrays;
+public class AscendingOrderDemo {
+	
+	public static void main(String args[]) {
+		Integer[]a1= new Integer[] {12,20,15,80,50,30};
+		Arrays.sort(a1);
+		System.out.println(Arrays.toString(a1));
+		
+		
+	}
+
+}
